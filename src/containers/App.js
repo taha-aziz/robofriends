8bf401c3eaeabce03b424d2833b2b5222b60e55a
Taha Aziz <25tahaaziz25@gmail.com>
2020-08-14 13:48:06 -0700

    migrate from vscode to git

---
+++ src/containers/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import CardList from '../components/CardList';
+import ErrorBoundry from '../components/ErrorBoundry';
+import './App.css';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            robots: [],
+            searchfield: ""
+        }
+    }
+
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({
+            searchfield: event.target.value
+        })
+    }
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        })
+        return !robots.length ?
+            <h1 className="tc">Loading...</h1> :
+            (
+                <div className="tc">
+                    <h1 className="f1">RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
+                    </Scroll>
+                </div>
+            )
+    }
+}
+
+export default App;
